219ca004000b976a51738ca1d131bd099f2eb1bc
Rubén Canas Moraleda <rcanmor@g.educaand.es>
2023-05-23 16:01:15 +0200

    Added confirmation dialog

--- lib/commons/custom_widgets/confirmation_dialog.dart
+++ lib/commons/custom_widgets/confirmation_dialog.dart
@@ -1,1 +1,42 @@
-// TODO create
+import 'package:flutter/material.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const AlertDialog();
+  }
+}
+
+showAlertDialog(BuildContext context,
+    {required Function onConfirm,
+    String title = "Confirmation",
+    String cancelText = "Cancel",
+    String acceptText = "Yes",
+    String message = "Are you sure?"}) {
+  AlertDialog alert = AlertDialog(
+    title: Text(title),
+    content: Text(message),
+    actions: [
+      TextButton(
+        onPressed: () {
+          Navigator.of(context).pop();
+        },
+        child: Text(cancelText),
+      ),
+      TextButton(
+          onPressed: () {
+            onConfirm();
+            Navigator.of(context).pop();
+          },
+          child: Text(acceptText))
+    ],
+  );
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return alert;
+    },
+  );
+}
